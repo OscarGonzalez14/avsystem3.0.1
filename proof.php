ded2bb80a401713bc0c47a8293e8e34a2abdf1c8
OscarGonzalez14 <oscargonzalez815@gmail.com>
2020-12-30 09:25:16 -0600

    he agregado cambios en proof.htm

--- proof.php
+++ proof.php
@@ -12,3 +12,14 @@ $fecha_actual = date("d-m-Y");
 echo date("d-m-Y",strtotime($fecha_actual."+ 1 month"))."<br>"; 
 //resto 1 mes
 echo date("d-m-Y",strtotime($fecha_actual."- 1 month"))."<br>";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+ 	Hemos agregadpo codigo HTML al archivo
+</body>
+</html>
